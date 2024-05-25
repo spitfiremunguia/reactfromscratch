6daf8b1914f590fbab4d90f0bf333dfec8931444
spitfiremunguia <damunguia@protonmail.com>
2024-05-25 12:44:41 -0600

    import/export default example

--- js-refresher/assets/scripts/utils.js
+++ js-refresher/assets/scripts/utils.js
@@ -1,3 +1,8 @@
-export let helloWorld = ()=>{
+//with default I don't need to set a name to this value
+export default  ()=>{
     alert("Hello World");
+};
+
+export let byeBye = ()=>{
+    alert("Bye Bye");
 };
--- js-refresher/index.js
+++ js-refresher/index.js
@@ -1,3 +1,5 @@
-import { helloWorld } from "./assets/scripts/utils.js";
+import helloWorld from "./assets/scripts/utils.js";
+import { byeBye } from "./assets/scripts/utils.js";
 
 helloWorld();
+byeBye();
